19c03a95226b7113e88ba8de4104e52b4801a257
Дмитрий Шапченко <dmitryshapchenko@yandex.ru>
2022-08-16 18:54:10 +0300

    sprint 5

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from posts.models import Post, Group
+from http import HTTPStatus
 
 User = get_user_model()
 
@@ -35,29 +36,29 @@ class ContactURLTests(TestCase):
     def test_index_url(self):
         """Главная страница для неавторизированного пользователя"""
         response = self.guest.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_group_url(self):
         """Страница /group/<slug>/ для неавторизированного пользователя"""
         response = self.guest.get('/group/test-slug/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_profile_url(self):
         """Страница profile для неавторизированного пользователя"""
         response = self.guest.get('/profile/test/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_posts_url(self):
         """Страница /posts/<post_id>/ для неавторизированного пользователя"""
         post_id = self.post.id
         response = self.guest.get(f'/posts/{post_id}/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_posts_edit_url(self):
         """Страница /posts/<post_id>/edit/ для автора"""
         post_id = self.post.id
         response = self.user_author.get(f'/posts/{post_id}/edit/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_posts_edit_url(self):
         """Редирект после редактирования поста для неавт. польз."""
@@ -71,7 +72,7 @@ class ContactURLTests(TestCase):
     def test_create_url_authoritized(self):
         """Страница /create/ для авторизированного пользователя"""
         response = self.authoritized_user.get('/create/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_create_url(self):
         """Проверка /create/ для неавторизированного пользователя"""
@@ -83,7 +84,7 @@ class ContactURLTests(TestCase):
     def test_unexisting_page(self):
         """Несуществующая страница, ошибка 404"""
         response = self.guest.get('/unexisting_page/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_templates(self):
         """Проверяем шаблоны"""
