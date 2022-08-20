808aa23087e6e9c8b7f85dc1362409d7ef865911
Дмитрий Шапченко <dmitryshapchenko@yandex.ru>
2022-08-20 18:10:14 +0300

    review 5

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -21,19 +21,6 @@ class PostViewTests(TestCase):
         super().setUpClass()
         """Создаем пользователя,группу, пост"""
         cls.user = User.objects.create_user(username='test')
-        cls.small_gif = (
-            b'\x47\x49\x46\x38\x39\x61\x02\x00'
-            b'\x01\x00\x80\x00\x00\x00\x00\x00'
-            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-            b'\x0A\x00\x3B'
-        )
-        cls.uploaded = SimpleUploadedFile(
-            name='small.gif',
-            content=cls.small_gif,
-            content_type='image/gif'
-        )
         cls.group = Group.objects.create(
             title='Тестовое название',
             slug='test-slug',
@@ -43,13 +30,7 @@ class PostViewTests(TestCase):
             text='Тестовый текст',
             author=cls.user,
             group=cls.group,
-            image=cls.uploaded
         )
-
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
-        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         """Пользователь"""
