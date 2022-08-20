0739378bef54cc20e0fef6bfd9ad8791baa9b5ea
Дмитрий Шапченко <dmitryshapchenko@yandex.ru>
2022-08-20 18:12:17 +0300

    review 5

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,8 +1,6 @@
-import shutil
 import tempfile
 from django.contrib.auth import get_user_model
 from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, Client, override_settings
 from posts.models import Post, Group
 from django.urls import reverse
